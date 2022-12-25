caeae5a7676e2fc9575c0b3860f306bf4eabb4aa
voidedWarranties <hi+github@voidedxd.xyz>
2022-12-24 18:01:11 -0800

    Remove all unnecessary virtual declarations
    How embarrassing

--- src/luau_script.h
+++ src/luau_script.h
@@ -51,54 +51,54 @@ protected:
 public:
     Ref<LuauScript> base; // !! public for tests only !!
 
-    virtual bool _has_source_code() const override;
-    virtual String _get_source_code() const override;
-    virtual void _set_source_code(const String &p_code) override;
+    bool _has_source_code() const override;
+    String _get_source_code() const override;
+    void _set_source_code(const String &p_code) override;
     Error load_source_code(const String &p_path);
-    virtual Error _reload(bool p_keep_state) override;
+    Error _reload(bool p_keep_state) override;
 
-    virtual ScriptLanguage *_get_language() const override;
+    ScriptLanguage *_get_language() const override;
 
-    virtual bool _is_valid() const override;
-    virtual bool _can_instantiate() const override;
+    bool _is_valid() const override;
+    bool _can_instantiate() const override;
 
     /* SCRIPT INFO */
-    virtual bool _is_tool() const override;
-    virtual StringName _get_instance_base_type() const override;
-    virtual Ref<Script> _get_base_script() const override;
-    virtual bool _inherits_script(const Ref<Script> &p_script) const override;
+    bool _is_tool() const override;
+    StringName _get_instance_base_type() const override;
+    Ref<Script> _get_base_script() const override;
+    bool _inherits_script(const Ref<Script> &p_script) const override;
 
-    virtual TypedArray<Dictionary> _get_script_method_list() const override;
-    virtual bool _has_method(const StringName &p_method) const override;
+    TypedArray<Dictionary> _get_script_method_list() const override;
+    bool _has_method(const StringName &p_method) const override;
     bool has_method(const StringName &p_method, StringName *r_actual_name = nullptr) const;
-    virtual Dictionary _get_method_info(const StringName &p_method) const override;
+    Dictionary _get_method_info(const StringName &p_method) const override;
 
-    virtual TypedArray<Dictionary> _get_script_property_list() const override;
-    virtual TypedArray<StringName> _get_members() const override;
-    virtual bool _has_property_default_value(const StringName &p_property) const override;
-    virtual Variant _get_property_default_value(const StringName &p_property) const override;
+    TypedArray<Dictionary> _get_script_property_list() const override;
+    TypedArray<StringName> _get_members() const override;
+    bool _has_property_default_value(const StringName &p_property) const override;
+    Variant _get_property_default_value(const StringName &p_property) const override;
 
     bool has_property(const StringName &p_name) const;
     const GDClassProperty &get_property(const StringName &p_name) const;
 
     /* INSTANCE */
-    virtual void *_instance_create(Object *p_for_object) const override;
-    virtual bool _instance_has(Object *p_object) const override;
+    void *_instance_create(Object *p_for_object) const override;
+    bool _instance_has(Object *p_object) const override;
     LuauScriptInstance *instance_get(Object *p_object) const;
 
     /*
-    virtual void _placeholder_erased(void *placeholder);
-    virtual void *_placeholder_instance_create(Object *for_object) const;
-    virtual bool _has_script_signal(const StringName &signal) const;
-    virtual TypedArray<Dictionary> _get_script_signal_list() const;
-    virtual void _update_exports();
-    virtual int64_t _get_member_line(const StringName &member) const;
-    virtual Dictionary _get_constants() const;
-    virtual bool _is_placeholder_fallback_enabled() const;
-    virtual Variant _get_rpc_config() const;
+    void _placeholder_erased(void *placeholder);
+    void *_placeholder_instance_create(Object *for_object) const;
+    bool _has_script_signal(const StringName &signal) const;
+    TypedArray<Dictionary> _get_script_signal_list() const;
+    void _update_exports();
+    int64_t _get_member_line(const StringName &member) const;
+    Dictionary _get_constants() const;
+    bool _is_placeholder_fallback_enabled() const;
+    Variant _get_rpc_config() const;
 
     // To implement later (or never)
-    virtual TypedArray<Dictionary> _get_documentation() const;
+    TypedArray<Dictionary> _get_documentation() const;
     */
 
     void def_table_get(GDLuau::VMType p_vm_type, lua_State *T) const;
@@ -193,94 +193,94 @@ protected:
 public:
     static LuauLanguage *get_singleton() { return singleton; }
 
-    virtual void _init() override;
-    virtual void _finish() override;
+    void _init() override;
+    void _finish() override;
 
     /* LANGUAGE INFO */
-    virtual String _get_name() const override;
-    virtual String _get_type() const override;
+    String _get_name() const override;
+    String _get_type() const override;
 
-    virtual String _get_extension() const override;
-    virtual PackedStringArray _get_recognized_extensions() const override;
+    String _get_extension() const override;
+    PackedStringArray _get_recognized_extensions() const override;
 
-    virtual PackedStringArray _get_reserved_words() const override;
-    virtual bool _is_control_flow_keyword(const String &p_keyword) const override;
+    PackedStringArray _get_reserved_words() const override;
+    bool _is_control_flow_keyword(const String &p_keyword) const override;
 
-    virtual PackedStringArray _get_comment_delimiters() const override;
-    virtual PackedStringArray _get_string_delimiters() const override;
+    PackedStringArray _get_comment_delimiters() const override;
+    PackedStringArray _get_string_delimiters() const override;
 
-    virtual bool _supports_builtin_mode() const override;
+    bool _supports_builtin_mode() const override;
 
     /* ... */
-    virtual Object *_create_script() const override;
-    virtual Ref<Script> _make_template(const String &p_template, const String &p_class_name, const String &p_base_class_name) const override;
+    Object *_create_script() const override;
+    Ref<Script> _make_template(const String &p_template, const String &p_class_name, const String &p_base_class_name) const override;
 
     /* ???: pure virtual functions which have no clear purpose */
-    virtual Error _execute_file(const String &p_path) override;
-    virtual bool _has_named_classes() const override;
+    Error _execute_file(const String &p_path) override;
+    bool _has_named_classes() const override;
 
     /*
-    virtual Dictionary _validate(const String &script, const String &path, bool validate_functions, bool validate_errors, bool validate_warnings, bool validate_safe_lines) const;
-    virtual bool _can_inherit_from_file() const;
-    virtual int64_t _find_function(const String &class_name, const String &function_name) const;
-    virtual String _make_function(const String &class_name, const String &function_name, const PackedStringArray &function_args) const;
-    virtual void _reload_all_scripts();
-    virtual void _reload_tool_script(const Ref<Script> &script, bool soft_reload);
-    virtual void _frame();
+    Dictionary _validate(const String &script, const String &path, bool validate_functions, bool validate_errors, bool validate_warnings, bool validate_safe_lines) const;
+    bool _can_inherit_from_file() const;
+    int64_t _find_function(const String &class_name, const String &function_name) const;
+    String _make_function(const String &class_name, const String &function_name, const PackedStringArray &function_args) const;
+    void _reload_all_scripts();
+    void _reload_tool_script(const Ref<Script> &script, bool soft_reload);
+    void _frame();
 
     // To implement later (or never)
-    virtual void _add_global_constant(const StringName &name, const Variant &value);
-    virtual void _add_named_global_constant(const StringName &name, const Variant &value);
-    virtual void _remove_named_global_constant(const StringName &name);
+    void _add_global_constant(const StringName &name, const Variant &value);
+    void _add_named_global_constant(const StringName &name, const Variant &value);
+    void _remove_named_global_constant(const StringName &name);
 
-    virtual TypedArray<Dictionary> _get_built_in_templates(const StringName &object) const;
-    virtual bool _is_using_templates();
+    TypedArray<Dictionary> _get_built_in_templates(const StringName &object) const;
+    bool _is_using_templates();
 
-    virtual Error _open_in_external_editor(const Ref<Script> &script, int64_t line, int64_t column);
-    virtual bool _overrides_external_editor();
+    Error _open_in_external_editor(const Ref<Script> &script, int64_t line, int64_t column);
+    bool _overrides_external_editor();
 
-    virtual Dictionary _complete_code(const String &code, const String &path, Object *owner) const;
-    virtual Dictionary _lookup_code(const String &code, const String &symbol, const String &path, Object *owner) const;
-    virtual String _auto_indent_code(const String &code, int64_t from_line, int64_t to_line) const;
+    Dictionary _complete_code(const String &code, const String &path, Object *owner) const;
+    Dictionary _lookup_code(const String &code, const String &symbol, const String &path, Object *owner) const;
+    String _auto_indent_code(const String &code, int64_t from_line, int64_t to_line) const;
 
-    virtual void _thread_enter();
-    virtual void _thread_exit();
+    void _thread_enter();
+    void _thread_exit();
 
-    virtual String _validate_path(const String &path) const; // used by C# only to prevent naming class to keyword. probably not super necessary
+    String _validate_path(const String &path) const; // used by C# only to prevent naming class to keyword. probably not super necessary
 
     // Non-essential. For class icons?
-    virtual bool _handles_global_class_type(const String &type) const;
-    virtual Dictionary _get_global_class_name(const String &path) const;
+    bool _handles_global_class_type(const String &type) const;
+    Dictionary _get_global_class_name(const String &path) const;
 
     // Debugger
-    virtual String _debug_get_error() const;
-    virtual int64_t _debug_get_stack_level_count() const;
-    virtual int64_t _debug_get_stack_level_line(int64_t level) const;
-    virtual String _debug_get_stack_level_function(int64_t level) const;
-    virtual Dictionary _debug_get_stack_level_locals(int64_t level, int64_t max_subitems, int64_t max_depth);
-    virtual Dictionary _debug_get_stack_level_members(int64_t level, int64_t max_subitems, int64_t max_depth);
-    virtual void *_debug_get_stack_level_instance(int64_t level);
-    virtual Dictionary _debug_get_globals(int64_t max_subitems, int64_t max_depth);
-    virtual String _debug_parse_stack_level_expression(int64_t level, const String &expression, int64_t max_subitems, int64_t max_depth);
-    virtual TypedArray<Dictionary> _debug_get_current_stack_info();
+    String _debug_get_error() const;
+    int64_t _debug_get_stack_level_count() const;
+    int64_t _debug_get_stack_level_line(int64_t level) const;
+    String _debug_get_stack_level_function(int64_t level) const;
+    Dictionary _debug_get_stack_level_locals(int64_t level, int64_t max_subitems, int64_t max_depth);
+    Dictionary _debug_get_stack_level_members(int64_t level, int64_t max_subitems, int64_t max_depth);
+    void *_debug_get_stack_level_instance(int64_t level);
+    Dictionary _debug_get_globals(int64_t max_subitems, int64_t max_depth);
+    String _debug_parse_stack_level_expression(int64_t level, const String &expression, int64_t max_subitems, int64_t max_depth);
+    TypedArray<Dictionary> _debug_get_current_stack_info();
 
     // Profiler
-    virtual void _profiling_start();
-    virtual void _profiling_stop();
-    virtual int64_t _profiling_get_accumulated_data(ScriptLanguageExtensionProfilingInfo *info_array, int64_t info_max);
-    virtual int64_t _profiling_get_frame_data(ScriptLanguageExtensionProfilingInfo *info_array, int64_t info_max);
+    void _profiling_start();
+    void _profiling_stop();
+    int64_t _profiling_get_accumulated_data(ScriptLanguageExtensionProfilingInfo *info_array, int64_t info_max);
+    int64_t _profiling_get_frame_data(ScriptLanguageExtensionProfilingInfo *info_array, int64_t info_max);
 
     // For docs
-    virtual bool _supports_documentation() const;
-    virtual Array _get_public_functions() const;
-    virtual Dictionary _get_public_constants() const;
-    virtual TypedArray<Dictionary> _get_public_annotations() const;
+    bool _supports_documentation() const;
+    Array _get_public_functions() const;
+    Dictionary _get_public_constants() const;
+    TypedArray<Dictionary> _get_public_annotations() const;
 
     // Seemingly unused by Godot
-    virtual void *_alloc_instance_binding_data(Object *object);
-    virtual void _free_instance_binding_data(void *data);
-    virtual void _refcount_incremented_instance_binding(Object *object);
-    virtual bool _refcount_decremented_instance_binding(Object *object);
+    void *_alloc_instance_binding_data(Object *object);
+    void _free_instance_binding_data(void *data);
+    void _refcount_incremented_instance_binding(Object *object);
+    bool _refcount_decremented_instance_binding(Object *object);
     */
 
     LuauLanguage();
@@ -295,10 +295,10 @@ protected:
     static void _bind_methods() {}
 
 public:
-    virtual PackedStringArray _get_recognized_extensions() const override;
-    virtual bool _handles_type(const StringName &p_type) const override;
-    virtual String _get_resource_type(const String &p_path) const override;
-    virtual Variant _load(const String &p_path, const String &p_original_path, bool p_use_sub_threads, int64_t p_cache_mode) const override;
+    PackedStringArray _get_recognized_extensions() const override;
+    bool _handles_type(const StringName &p_type) const override;
+    String _get_resource_type(const String &p_path) const override;
+    Variant _load(const String &p_path, const String &p_original_path, bool p_use_sub_threads, int64_t p_cache_mode) const override;
 };
 
 class ResourceFormatSaverLuauScript : public ResourceFormatSaver
@@ -309,7 +309,7 @@ protected:
     static void _bind_methods() {}
 
 public:
-    virtual PackedStringArray _get_recognized_extensions(const Ref<Resource> &p_resource) const override;
-    virtual bool _recognize(const Ref<Resource> &p_resource) const override;
-    virtual int64_t _save(const Ref<Resource> &p_resource, const String &p_path, int64_t p_flags) override;
+    PackedStringArray _get_recognized_extensions(const Ref<Resource> &p_resource) const override;
+    bool _recognize(const Ref<Resource> &p_resource) const override;
+    int64_t _save(const Ref<Resource> &p_resource, const String &p_path, int64_t p_flags) override;
 };
